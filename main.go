a42813843c3da0afd0b338bd7f084ffc656b190a
Władysłąw Panin <105151413+vladosss7007@users.noreply.github.com>
2024-01-12 12:25:38 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,3 +1,22 @@
+// main.go
 package main
 
-func main()
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"main.go/handlers"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "templates/index.html")
+	})
+
+	http.HandleFunc("/api", handlers.APIHandler)
+
+	port := 8080
+	fmt.Printf("Server is running on :%d...\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
